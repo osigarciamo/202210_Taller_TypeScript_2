f7b8805d6386dd675c8af16d8b25e0376a082900
Oscar Ivan Garcia Montañez <oscarivangarciamontanez@MacBook-Air-de-Oscar.local>
2022-04-21 23:54:12 -0500

    Final

--- main.ts
+++ main.ts
@@ -2,10 +2,28 @@ import { Serie } from "./Serie.js";
 import { dataSeries } from "./Data.js";
 
 const seriesTbody: HTMLElement = document.getElementById('seriesTV')!; // Nodo tbody que tiene el id="seriesTV"
-const avgSeasonNumber: HTMLElement= document.getElementById('average')!;
+const seriesCardDiv: HTMLElement = document.getElementById('card-series')!;
+const avgSeasonNumber: HTMLElement= document.getElementById('avg-seasons')!;
+let nameButtons: HTMLCollectionOf<Element> = document.getElementsByClassName('btn-serie-name')!;
 
 renderSeriesInTable(dataSeries);
 avgSeasonNumber.innerHTML = `${seasonsAverage(dataSeries)}`
+
+for (let i = 0; i < nameButtons.length; i++) {
+    nameButtons[i].addEventListener("click", () => {
+        clearInfoCard();
+        renderSeriesInformation(dataSeries[i]);
+    });
+}
+
+/* Borra la informaci'on */
+function clearInfoCard() {
+    while (seriesCardDiv.hasChildNodes()) {
+        if (seriesCardDiv.firstChild != null) {
+            seriesCardDiv.removeChild(seriesCardDiv.firstChild);
+        }
+    }
+}
 
 /* Lee los datos de un arreglo de objetos series y finalmente lo despliega en la tabla de series actuales (DOM). */
 function renderSeriesInTable(series: Serie[]): void {
@@ -13,9 +31,24 @@ function renderSeriesInTable(series: Serie[]): void {
     series.forEach(serie => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td>${serie.id}</td> <td>${serie.name}</td> <td>${serie.channel}</td> <td>${serie.seasons}</td>`;
+        let infoElement = document.createElement("td");
+        infoElement.innerHTML = `<input type = "button" value = "${serie.name}" class="btn btn-link series-button"></button>`;
         seriesTbody.appendChild(trElement);
   });
 }
+/* Genera la informacion de la serie. */
+function renderSeriesInformation(serie: Serie): void{
+    console.log('Desplegando informacion');
+    let divElement = document.createElement("div");
+        divElement.innerHTML = `<img class='card-img-top' src="${serie.image}" alt='image-serie-${serie.name}'>
+                                <div class='card-body'>
+                                <h5 class='card-title'>${serie.name}</h5>
+                                <p class='card-text'>${serie.description}</p>
+                                <a href="${serie.review}">${serie.review}</a>
+                                </div>`;
+    seriesCardDiv.appendChild(divElement);
+}
+
 /* Calcula el promedio de las temporadas. */
 function seasonsAverage (series : Serie[]): number {
     console.log('Determinando el promedio de las series');
@@ -23,3 +56,5 @@ function seasonsAverage (series : Serie[]): number {
     series.forEach((Serie) => totalSeasons = totalSeasons + Serie.seasons);
     return (totalSeasons/ series.length);
 }
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,44 @@
 import { dataSeries } from "./Data.js";
 var seriesTbody = document.getElementById('seriesTV'); // Nodo tbody que tiene el id="seriesTV"
-var avgSeasonNumber = document.getElementById('average');
+var seriesCardDiv = document.getElementById('card-series');
+var avgSeasonNumber = document.getElementById('avg-seasons');
+var nameButtons = document.getElementsByClassName('btn-serie-name');
 renderSeriesInTable(dataSeries);
 avgSeasonNumber.innerHTML = "".concat(seasonsAverage(dataSeries));
+var _loop_1 = function (i) {
+    nameButtons[i].addEventListener("click", function () {
+        clearInfoCard();
+        renderSeriesInformation(dataSeries[i]);
+    });
+};
+for (var i = 0; i < nameButtons.length; i++) {
+    _loop_1(i);
+}
+/* Borra la informaci'on */
+function clearInfoCard() {
+    while (seriesCardDiv.hasChildNodes()) {
+        if (seriesCardDiv.firstChild != null) {
+            seriesCardDiv.removeChild(seriesCardDiv.firstChild);
+        }
+    }
+}
 /* Lee los datos de un arreglo de objetos series y finalmente lo despliega en la tabla de series actuales (DOM). */
 function renderSeriesInTable(series) {
     console.log('Desplegando programas de televisión y series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td> <td>").concat(serie.name, "</td> <td>").concat(serie.channel, "</td> <td>").concat(serie.seasons, "</td>");
+        var infoElement = document.createElement("td");
+        infoElement.innerHTML = "<input type = \"button\" value = \"".concat(serie.name, "\" class=\"btn btn-link series-button\"></button>");
         seriesTbody.appendChild(trElement);
     });
+}
+/* Genera la informacion de la serie. */
+function renderSeriesInformation(serie) {
+    console.log('Desplegando informacion');
+    var divElement = document.createElement("div");
+    divElement.innerHTML = "<img class='card-img-top' src=\"".concat(serie.image, "\" alt='image-serie-").concat(serie.name, "'>\n                                <div class='card-body'>\n                                <h5 class='card-title'>").concat(serie.name, "</h5>\n                                <p class='card-text'>").concat(serie.description, "</p>\n                                <a href=\"").concat(serie.review, "\">").concat(serie.review, "</a>\n                                </div>");
+    seriesCardDiv.appendChild(divElement);
 }
 /* Calcula el promedio de las temporadas. */
 function seasonsAverage(series) {
@@ -19,3 +47,4 @@ function seasonsAverage(series) {
     series.forEach(function (Serie) { return totalSeasons = totalSeasons + Serie.seasons; });
     return (totalSeasons / series.length);
 }
+
